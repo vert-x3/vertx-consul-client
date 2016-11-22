11744a2b07382ee650110f430012807e0f47bce9
Ruslan Sennov <ruslan.sennov@gmail.com>
2016-11-22 21:57:21 +0300

    unused imports removed; copyright headers added

--- vertx-consul-client/src/test/java/io/vertx/ext/consul/ConsulCluster.java
+++ vertx-consul-client/src/test/java/io/vertx/ext/consul/ConsulCluster.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016 The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
 package io.vertx.ext.consul;
 
 import com.pszymczyk.consul.ConsulProcess;
@@ -7,7 +22,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 import java.util.concurrent.CountDownLatch;
--- vertx-consul-client/src/test/java/io/vertx/ext/consul/suite/Catalog.java
+++ vertx-consul-client/src/test/java/io/vertx/ext/consul/suite/Catalog.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016 The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
 package io.vertx.ext.consul.suite;
 
 import io.vertx.ext.consul.ConsulTestBase;
@@ -5,7 +20,6 @@ import io.vertx.ext.consul.Node;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static io.vertx.ext.consul.Utils.getAsync;
 
