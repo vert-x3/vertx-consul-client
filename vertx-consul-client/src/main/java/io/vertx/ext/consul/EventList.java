11aa326838e31c97ed898db2701e29ecaedebd6f
Ruslan Sennov <ruslan.sennov@gmail.com>
2016-12-22 07:13:33 +0300

    copyright header

--- vertx-consul-client/src/main/java/io/vertx/ext/consul/EventList.java
+++ vertx-consul-client/src/main/java/io/vertx/ext/consul/EventList.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016 The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
 package io.vertx.ext.consul;
 
 import io.vertx.codegen.annotations.DataObject;
@@ -6,6 +21,8 @@ import io.vertx.core.json.JsonObject;
 import java.util.List;
 
 /**
+ * Holds result of events query
+
  * @author <a href="mailto:ruslan.sennov@gmail.com">Ruslan Sennov</a>
  */
 @DataObject(generateConverter = true)
