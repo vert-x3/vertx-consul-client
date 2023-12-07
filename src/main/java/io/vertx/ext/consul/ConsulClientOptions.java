2c72244d5b0a233623506eb47e65bf13ac2e5d25
Julien Viet <julien@julienviet.com>
2023-12-07 13:48:04 +0100

    Fix incorrect package for JsonGen annotation

--- src/main/java/io/vertx/ext/consul/ConsulClientOptions.java
+++ src/main/java/io/vertx/ext/consul/ConsulClientOptions.java
@@ -17,7 +17,7 @@ package io.vertx.ext.consul;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.annotations.GenIgnore;
-import io.vertx.codegen.annotations.JsonGen;
+import io.vertx.codegen.json.annotations.JsonGen;
 import io.vertx.core.VertxException;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.Http2Settings;
