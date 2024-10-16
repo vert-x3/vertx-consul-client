5abc6c2e91f7984eb72379e18fd890a95ac82bca
paras soni <parasso@amdocs.com>
2024-10-16 07:09:31 +0000

    file added

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,10 +1,10 @@
-open module io.vertx.consul.client.tests {
+open module io.vertx.consul.client {
   requires consul;
   requires com.github.dockerjava.api;
   requires com.fasterxml.jackson.annotation;
   requires io.vertx.core;
   requires io.vertx.consul.client;
-  requires io.vertx.core.tests;
+  requires io.vertx.core;
   requires io.vertx.testing.unit;
   requires junit;
   requires hamcrest.core;
